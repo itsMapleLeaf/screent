1adfcb6f2ad2a6608be878def37c5c3b307cb7ee
itsMapleLeaf <19603573+itsMapleLeaf@users.noreply.github.com>
2022-04-13 12:07:46 -0500

    use electron-builder prebuild hook

---
+++ electron-builder.cjs
@@ -0,0 +1,35 @@
+const { mkdir } = require("node:fs/promises")
+const { relative, resolve } = require("node:path")
+const sharp = require("sharp")
+
+/** @type {import('electron-builder').Configuration} */
+const config = {
+  appId: "dev.mapleleaf.screent",
+  files: ["{build,public}/**/*"],
+  asar: false,
+  directories: {
+    output: "release",
+  },
+  linux: {
+    target: ["AppImage", "flatpak"],
+    icon: "assets/icons/linux",
+    category: "Utility",
+    publish: ["github"],
+  },
+  beforePack: async () => {
+    const projectRoot = __dirname
+    const iconFile = resolve(projectRoot, "public/icon.png")
+    const iconSizes = [16, 24, 32, 48, 64, 128, 256, 512]
+    const outputFolder = resolve(projectRoot, "assets/icons/linux")
+
+    await mkdir(outputFolder, { recursive: true })
+
+    for (const size of iconSizes) {
+      const outputFile = resolve(outputFolder, `${size}x${size}.png`)
+      await sharp(iconFile).resize(size, size).toFile(outputFile)
+      console.info(`Created`, relative(projectRoot, outputFile))
+    }
+  },
+}
+
+module.exports = config
